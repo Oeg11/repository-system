bef5b5fdef43947bd38cfe9ebbee9977b55c2d4f
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:42:54 +0800

    sd

--- resources/views/admin/alayouts/sidebar.blade.php
+++ resources/views/admin/alayouts/sidebar.blade.php
@@ -97,7 +97,7 @@
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
-            <li class="nav-item menu-open">
+            <li class="nav-item">
               <a href="{{route('admin.dashboard')}}"  class="nav-link btn">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
