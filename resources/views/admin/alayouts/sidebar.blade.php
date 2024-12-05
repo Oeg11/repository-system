a5452d5dce98269b288241614217fe94a470ba2c
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:46:51 +0800

    sdfsdf

--- resources/views/admin/alayouts/sidebar.blade.php
+++ resources/views/admin/alayouts/sidebar.blade.php
@@ -188,8 +188,7 @@
         var header = document.getElementById("myDIV");
         var btns = header.getElementsByClassName("btns");
         for (var i = 0; i < btns.length; i++) {
-          btns[i].addEventListener("click", function(e) {
-            e.preventDefault();
+          btns[i].addEventListener("click", function() {
           var current = document.getElementsByClassName("active");
           current[0].className = current[0].className.replace(" active", "");
           this.className += " active";
