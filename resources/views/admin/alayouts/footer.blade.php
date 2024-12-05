ed0309793a90bbfedca09e89b39776f2a8814f86
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:03:56 +0800

    df

--- resources/views/admin/alayouts/footer.blade.php
+++ resources/views/admin/alayouts/footer.blade.php
@@ -1,9 +1,3 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2024-2025 <a href="#">STI Repository</a>.</strong> All rights reserved.
-  </footer>
 
 
   <!-- Control Sidebar -->
--- resources/views/admin/studentlistsgoogleauth.blade.php
+++ resources/views/admin/studentlistsgoogleauth.blade.php
@@ -61,6 +61,12 @@
     </section>
     <!-- /.content -->
   </div>
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2024-2025 <a href="#">STI Repository</a>.</strong> All rights reserved.
+  </footer>
 
  @include('admin.modal.viewstudent')
  @include('admin.modal.editstudentstatus')
