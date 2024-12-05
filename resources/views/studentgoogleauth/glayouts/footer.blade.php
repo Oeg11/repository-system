9bb07ecd9d08415a103cd88d1ba40b043480ccca
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:29:40 +0800

    sad

--- resources/views/studentgoogleauth/glayouts/footer.blade.php
+++ resources/views/studentgoogleauth/glayouts/footer.blade.php
@@ -1,11 +1,11 @@
 
-<footer class="main-footer">
+{{-- <footer class="main-footer">
     <strong>Copyright &copy; 2024 <a href="#">STI Repository</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
     </div>
-  </footer>
+  </footer> --}}
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
