f58376334dabf92996501628dc1b3170a25edf48
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:20:19 +0800

    fgh

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -1,49 +1,72 @@
 @extends('admin.alayouts.main')
 @section('content')
 
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-          </div><!-- /.col -->
+            <h1>Dashboard</h1>
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('admin.dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
             </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
+          </div>
+        </div>
       </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    </section>
 
-
+    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        <h2>List of Settings</h2><hr>
-        <table class="table table-bordered data-table6">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">System Name</th>
-                    <th scope="col">System Short Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">About</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Contact #</th>
-                    <th scope="col">Address</th>
-                    <th width="100px">Action</th>
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <div class="row">
+                    <div class="col-md-10">List of Settings</h2></div>
 
-                </tr>
-            </thead>
-             <tbody>
-               {{-- content here --}}
-            </tbody>
-        </table>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="table-responsive">
+                <table class="table table-bordered table-hover data-table6">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">System Name</th>
+                            <th scope="col">System Short Name</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">About</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Contact #</th>
+                            <th scope="col">Address</th>
+                            <th width="100px">Action</th>
+
+                        </tr>
+                    </thead>
+                     <tbody>
+                    </tbody>
+                </table>
+              </div>
+            </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
       </div>
+      <!-- /.container-fluid -->
     </section>
+    <!-- /.content -->
   </div>
 
  @include('admin.modal.editsettings')
