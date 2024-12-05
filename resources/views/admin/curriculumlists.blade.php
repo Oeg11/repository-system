bbef0235747095d55e6c3b522a0899aa72e8e1b5
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:48:11 +0800

    df

--- resources/views/admin/curriculumlists.blade.php
+++ resources/views/admin/curriculumlists.blade.php
@@ -12,7 +12,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('admin.dashboard')}}">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
+              <li class="breadcrumb-item">Dashboard</li>
             </ol>
           </div>
         </div>
