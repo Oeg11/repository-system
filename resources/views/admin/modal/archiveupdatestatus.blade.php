983fd300a0e0da71679dd6f2b807b3f204a6cfc5
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-05 20:37:25 +0800

    sdf

--- resources/views/admin/modal/archiveupdatestatus.blade.php
+++ resources/views/admin/modal/archiveupdatestatus.blade.php
@@ -38,18 +38,14 @@
   </div>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script type="text/javascript">
- $(document).ready(function(){
-    $("#edit_status").change(function () {
-        var selected_option = $('#fnivel').val();
-        if (selected_option === '1') {
-            $('#remark').hide();
-        }
-        if (selected_option === '0') {
-            $("#remark").show();
-        }
+<script type="text/javascript">
+    $(function () {
+        $("#edit_status").change(function () {
+            if ($(this).val() === "0") {
+                $("#remark").show();
+            } else {
+                $("#remark").hide();
+            }
+        });
     });
-
-});
-
 </script>
