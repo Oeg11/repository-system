fb13c38912f93eeb9b51b5ade5dc80881ebdad56
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-05 19:59:12 +0800

    df
    dsf

--- resources/views/studentgoogleauth/glayouts/header.blade.php
+++ resources/views/studentgoogleauth/glayouts/header.blade.php
@@ -34,5 +34,19 @@
   <script src="https://cdn.ckeditor.com/ckeditor5/37.0.1/classic/ckeditor.js"></script>
 
 
+  <style>
+    #loader{
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('{{ asset("assets/img/output-onlinegiftools.gif") }}') 50% 50% no-repeat rgb(249,249,249);
+    opacity: 1;
+}
+</style>
+
+
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
