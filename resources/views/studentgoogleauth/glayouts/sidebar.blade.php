f4c0cfaf038c0a0659b9719221363018e922090e
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 01:09:57 +0800

    fg

--- resources/views/studentgoogleauth/glayouts/sidebar.blade.php
+++ resources/views/studentgoogleauth/glayouts/sidebar.blade.php
@@ -100,7 +100,7 @@
 
 
         <!-- Sidebar Menu -->
-        <nav class="mt-2">
+        <nav class="mt-2" id="myDIV">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
