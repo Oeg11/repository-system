f2c761fba84ba622afe00580a3b595d7b67cfd43
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:02:25 +0800

    sda

--- resources/views/admin/studentlistsgoogleauth.blade.php
+++ resources/views/admin/studentlistsgoogleauth.blade.php
@@ -62,14 +62,11 @@
     <!-- /.content -->
   </div>
 
-
-    <!-- /.content-wrapper -->
-
  @include('admin.modal.viewstudent')
  @include('admin.modal.editstudentstatus')
 
 
-
+  @endsection
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
