38deb068dba8efdf6ddf1eecbe0e36707c5c0f7f
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 01:59:56 +0800

    sdf

--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -58,7 +58,7 @@
 
 
   @endsection
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  {{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script>
