99d236703a1b667d1325d2df6313c80e735f207b
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:16:29 +0800

    dsf

--- resources/views/admin/curriculumlists.blade.php
+++ resources/views/admin/curriculumlists.blade.php
@@ -1,51 +1,70 @@
 @extends('admin.alayouts.main')
 @section('content')
 
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-          </div><!-- /.col -->
+            <h1>Dashboard</h1>
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('admin.dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
             </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
+          </div>
+        </div>
       </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    </section>
 
-
+    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-            <div class="col-md-10"><h2>List of Curriculum</h2></div>
-            <div class="col-md-2"><button type="button" class="btn btn-success"
-                 data-toggle="modal" data-target="#modal-addcurriculum">Add Curriculum</button></div>
-            @include('admin.modal.addcurriculum')
-        </div>
-        <hr>
-        <table class="table table-bordered data-table4">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Departmant</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th width="100px">Action</th>
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                 <div class="row">
+                    <div class="col-md-10"><h2>List of Curriculum</h2></div>
+                    <div class="col-md-2"><button type="button" class="btn btn-success"
+                         data-toggle="modal" data-target="#modal-addcurriculum">Add Curriculum</button></div>
+                    @include('admin.modal.addcurriculum')
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="table-responsive">
+                <table class="table table-bordered table-hover data-table4">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Departmant</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Description</th>
+                            <th width="100px">Action</th>
 
-                </tr>
-            </thead>
-             <tbody>
-               {{-- content here --}}
-            </tbody>
-        </table>
+                        </tr>
+                    </thead>
+                     <tbody>
+                    </tbody>
+                </table>
+              </div>
+            </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
       </div>
+      <!-- /.container-fluid -->
     </section>
+    <!-- /.content -->
   </div>
 
  @include('admin.modal.editcurriculum')
