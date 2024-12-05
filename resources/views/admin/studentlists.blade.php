ee3777238e9aab7bfcf76cc315310710d147c49d
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-05 23:34:56 +0800

    sad

--- resources/views/admin/studentlists.blade.php
+++ resources/views/admin/studentlists.blade.php
@@ -50,7 +50,7 @@
 
 
   @endsection
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  {{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 
