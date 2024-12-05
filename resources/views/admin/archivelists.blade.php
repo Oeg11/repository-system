1aacbf216e4ff865e58de013de41b11993974b0f
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-06 00:08:50 +0800

    sdf

--- resources/views/admin/archivelists.blade.php
+++ resources/views/admin/archivelists.blade.php
@@ -1,51 +1,72 @@
 @extends('admin.alayouts.main')
 @section('content')
 
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-          </div><!-- /.col -->
+            <h1>Dashboard</h1>
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard')}}">Home</a></li>
-              </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
+              <li class="breadcrumb-item"><a href="{{ route('admin.dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div>
+        </div>
       </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    </section>
 
-
+    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                 <h2>List of Collection</h2>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="table-responsive">
+                <table class="table table-bordered table-hover data-table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Date Created</th>
+                            <th scope="col">Archive Code</th>
+                            {{-- <th scope="col">Category</th> --}}
+                            <th scope="col">Project Title</th>
+                            <th scope="col">Department</th>
+                            <th scope="col">Curriculum</th>
+                            <th scope="col">Status</th>
+                            <th width="100px">Action</th>
+                        </tr>
+                    </thead>
+                     <tbody>
+                    </tbody>
+                </table>
+              </div>
+            </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
 
-        <table class="table table-bordered data-table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Date Created</th>
-                    <th scope="col">Archive Code</th>
-                    {{-- <th scope="col">Category</th> --}}
-                    <th scope="col">Project Title</th>
-                    <th scope="col">Department</th>
-                    <th scope="col">Curriculum</th>
-                    <th scope="col">Status</th>
-                    <th width="100px">Action</th>
-
-                </tr>
-            </thead>
-             <tbody>
-               {{-- content here --}}
-            </tbody>
-        </table>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
       </div>
+      <!-- /.container-fluid -->
     </section>
+    <!-- /.content -->
   </div>
+
 
   @include('admin.modal.viewarchive')
  @include('admin.modal.archiveupdatestatus')
