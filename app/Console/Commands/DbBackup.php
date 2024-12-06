8cf65886dab54f756093bec62203fb21715320ef
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 19:08:55 +0800

    dsff

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -37,7 +37,7 @@ class DbBackup extends Command
 
         $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD')
                 . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE')
-                . "  | gzip > " . public_path('/storage/uploads') . $filename;
+                . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
 
         $returnVar = NULL;
         $output  = NULL;
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,11 +7,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-
-        \App\Console\Commands\DbBackup::class,
-
-    ];
 
     protected function schedule(Schedule $schedule): void
     {
