83108b2c4e470b22b65cdf18c11dbde2aa9f3601
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 21:26:45 +0800

    sda

--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -108,7 +108,8 @@
                                             <label for="To">To</label>
                                             <input type="date" id="to" name="to" class="form-control" />
                                         </div>
-                                        <div class="col-md-3 mt-3">
+                                        <div class="col-md-3">
+                                            <label  style="color:white">x</label>
                                             <input type="button" class="btn btn-success" value="Filter" onclick="getData()" />
                                         </div>
                                     </div>
