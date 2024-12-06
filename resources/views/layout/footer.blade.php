d69f1cf26ff11019ccf236f419c6d9574f964943
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 18:34:30 +0800

    dsf

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -20,7 +20,7 @@
 
                     <ul class="liste-unstyled pull-right">
 
-                        <li><a href="javascript:void(0);" data-toggle="modal" data-target="#termsandcondition">Terms and Condition</a></li>
+                        <li><a href="javascript:void(0);" data-toggle="modal" data-target="#termsandcondition" style="text-decoration:none;color:white">Terms and Condition</a></li>
 
                         {{-- <li><a href="#twitter"><i class="fa fa-twitter"></i></a></li>
 
