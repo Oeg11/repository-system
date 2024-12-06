fab63adfba9b82382de641bfc879034e75fb36d6
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 21:49:11 +0800

    sad

--- resources/views/admin/alayouts/footer.blade.php
+++ resources/views/admin/alayouts/footer.blade.php
@@ -118,7 +118,7 @@
 <script>
     $(function () {
 
-    //     var charts =  {{ Js::from($charts) }};
+
 
     //   var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
     //   var donutData        = {
