5eaa8901288f9287ae1f5378b8e9e398e56dcfb0
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-07 03:14:18 +0800

    sda

--- resources/views/admin/backupdatabase.blade.php
+++ resources/views/admin/backupdatabase.blade.php
@@ -50,7 +50,7 @@
                         <tr>
                             <td>{{ $row->date_store }} </td>
                             <td>{{ asset('$row->database_path') }} </td>
-                            <td><a href="{{ $row->database_path }}" class="btn btn-success" download >Download Database</a></td>
+                            <td><a href="{{ asset('$row->database_path') }}" class="btn btn-success" download >Download Database</a></td>
                             @empty
 
                             @endforelse
