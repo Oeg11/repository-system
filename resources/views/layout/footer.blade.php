a0158cbf8a8692a05bd0c0f6a081ed49c5b4b02d
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 18:05:02 +0800

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -33,7 +33,7 @@
 
     </footer>
 
-     @include('moda.termsandcondition')
+     @include('modal.termsandcondition')
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
