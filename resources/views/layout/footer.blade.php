b4b6b20e3f3ae5aaea27dbf06532ddc5d8ad46fc
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 18:02:11 +0800

    dsfsf

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,23 +1,3 @@
-<!-- Button trigger modal -->
-
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
 
     <footer>
 
@@ -34,11 +14,8 @@
                     <h5 class="pull-left">STI Marikina &copy;2024 All rights reserved</h5>
 
                     <ul class="liste-unstyled pull-right">
-                       <li><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Terms and Condition
-                      </button>
-                      </li>
-                        {{-- <li><a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#exampleModal">Terms and Condition</a></li> --}}
+
+                        <li><a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#exampleModal">Terms and Condition</a></li>
 
                         {{-- <li><a href="#twitter"><i class="fa fa-twitter"></i></a></li>
 
@@ -56,11 +33,32 @@
 
     </footer>
 
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
