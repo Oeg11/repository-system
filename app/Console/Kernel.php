6662fad7a9135ed79376190cd909d646e6398e74
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 15:23:22 +0800

    df

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,9 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
+    protected $commands = [
+
+        'App\Console\Commands\DbBackup'
+
+    ];
+
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('db:backup')->everyMinute();
