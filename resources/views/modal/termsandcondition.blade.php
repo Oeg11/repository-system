1bd97b48a0ab5fa257066dccbaaf92cc787a0d05
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 18:18:36 +0800

    dsf

--- resources/views/modal/termsandcondition.blade.php
+++ resources/views/modal/termsandcondition.blade.php
@@ -3,13 +3,26 @@
         <div class="modal-dialog modal-dialog-scrollable" role="document">
         <div class="modal-content modal-xl">
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Terms And Condition</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
             </div>
             <div class="modal-body">
-            ...
+               <p>
+                Terms and Conditions for the Web-Based Repository of Projects, Theses, and Practical Research 
+                (STI Marikina)<br>
+                  Effective Date: [Insert Date]<br><br>
+                                    The STI Marikina Web-Based Repository is established to store, manage, and provide access to student 
+                    projects, theses, and research outputs for academic purposes. These rules and regulations aim to 
+                    maintain the integrity, security, and proper use of the Repository. <br><br>
+                    1. General Use <br>
+                    1.1. This Repository is provided by STI Marikina as an academic platform to store, share, and access 
+                    student projects, theses, and research outputs. <br>
+                    1.2. Users may only access the Repository for educational, research, and non-commercial purposes. <br>
+                    1.3. The content of the Repository is protected by copyright laws and may not be used beyond the 
+                   scope allowed by these Terms and Conditions. <br> 
+               </p>
             </div>
             <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
