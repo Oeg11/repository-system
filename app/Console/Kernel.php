6e20a6f7643dc24caf831fd351e5069608b6d451
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-06 19:14:55 +0800

    sad

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+
+        \App\Console\Commands\DbBackup::class,
+
+    ];
 
     protected function schedule(Schedule $schedule): void
     {
