cfc4424192d47bac4c2fb307a3338848bb61692f
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-09 06:21:04 +0800

    fghfgh

--- resources/views/students/slayouts/footer.blade.php
+++ resources/views/students/slayouts/footer.blade.php
@@ -203,7 +203,7 @@
            console.log("========================student_foreign_id========================");
            console.log(student_foreign_id);
 
-           var click_checkbox =  $('#click_checkbox').is(':checked') ? 1 : 0;
+           var click_checkbox =  $('#click_checkbox').is(':checked') ? "true" : "false";
            console.log("========================click_checkbox========================");
            console.log(click_checkbox);
 
@@ -280,7 +280,7 @@
                                         $('#student_id-error').html(response.errors.student_id[0]);
                                       }
 
-                                      if(response.errors.click_checkbox == 1){
+                                      if(response.errors.click_checkbox ==="true"){
                                          $('#check-error').hide();
                                       }else{
                                         $('#check-error').html("Please click a Term and Privacy Policy");
