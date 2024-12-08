7c33bc75de680847898b362c9db9777b817f9c72
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-08 19:46:51 +0800

    dawd
    dada

---
+++ resources/views/admin/import.blade.php
@@ -0,0 +1,32 @@
+@extends('admin.alayouts.main')
+@section('content')
+
+
+<div class="content-wrapper">
+    hello world
+
+  </div>
+
+  @endsection
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+
+  {{-- <script type="text/javascript">
+
+  $(document).ready(function(){
+
+    //view all archive
+
+      var table = $('.data-table34').DataTable({
+      });
+
+         //end view all archive
+
+
+
+
+
+    });
+
+  </script> --}}
