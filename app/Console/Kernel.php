eb3873e5e46d6646e2b73fe976a96b057f35f1c6
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-08 11:43:46 +0800

    dsd
    dsds

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('db:backup')->everyMinute();
+        $schedule->command('db:backup')->weekly();
     }
 
     /**
