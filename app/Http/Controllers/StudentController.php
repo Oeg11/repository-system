6e1ea750cd8dee52bde13aa36a74a02ba7d60aa4
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-09 06:17:38 +0800

    dfgfdg

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -82,59 +82,59 @@ class StudentController extends Controller
 
    public function submitProject(Request $request){
 
-    $validatedData = $request->validate([
-        'type' =>'required',
-        'category' =>'required',
-        'department_id' =>'required',
-        'curriculum_id' =>'required',
-        'title' =>'required',
-        'year' =>'required',
-        'abstract' =>'required',
-        'members' =>'required',
-        'adviser' =>'required',
-        'banner_path' =>'required|mimes:png,jpg,jpeg|max:2048',
-        'document_path' =>'required',
-        'click_checkbox' => 'required|boolean',
-    ]);
+    // $validatedData = $request->validate([
+    //     'type' =>'required',
+    //     'category' =>'required',
+    //     'department_id' =>'required',
+    //     'curriculum_id' =>'required',
+    //     'title' =>'required',
+    //     'year' =>'required',
+    //     'abstract' =>'required',
+    //     'members' =>'required',
+    //     'adviser' =>'required',
+    //     'banner_path' =>'required|mimes:png,jpg,jpeg|max:2048',
+    //     'document_path' =>'required',
+    //     'click_checkbox' => 'required|boolean',
+    // ]);
 
-    return response()->json([
-        'errors' => $validatedData,
-    ]);
+    // return response()->json([
+    //     'errors' => $validatedData,
+    // ]);
 
-        //  $validator = Validator::make($request->all(), [
+         $validator = Validator::make($request->all(), [
 
-        //         'type' =>'required',
-        //         'category' =>'required',
-        //         'department_id' =>'required',
-        //         'curriculum_id' =>'required',
-        //         'title' =>'required',
-        //         'year' =>'required',
-        //         'abstract' =>'required',
-        //         'members' =>'required',
-        //         'adviser' =>'required',
-        //         'banner_path' =>'required|mimes:png,jpg,jpeg|max:2048',
-        //         'document_path' =>'required',
-        //         'click_checkbox' =>'required',
-        //     ],[
+                'type' =>'required',
+                'category' =>'required',
+                'department_id' =>'required',
+                'curriculum_id' =>'required',
+                'title' =>'required',
+                'year' =>'required',
+                'abstract' =>'required',
+                'members' =>'required',
+                'adviser' =>'required',
+                'banner_path' =>'required|mimes:png,jpg,jpeg|max:2048',
+                'document_path' =>'required',
+                'click_checkbox' => 'required|boolean',
+            ],[
 
-        //         'type.required' => 'Please select type',
-        //         'category.required' => 'Please select category',
-        //         'department_id.required' => 'Please select your Department',
-        //         'curriculum_id.required' => 'Please select your Curriculum',
-        //         'title.required' => 'Please input unique Title',
-        //         'year.required' => 'Please select Year',
-        //         'abstract.required' => 'Please enter Abstract',
-        //         'members.required' => 'Please enter members',
-        //         'adviser.required' => 'Please input your Adviser',
-        //         'banner_path.required' => 'Please upload sample Image',
-        //         'document_path.required' => 'Please Attached document',
-        //         'click_checkbox.required' => 'Please click a Term and Privacy Policy'
+                'type.required' => 'Please select type',
+                'category.required' => 'Please select category',
+                'department_id.required' => 'Please select your Department',
+                'curriculum_id.required' => 'Please select your Curriculum',
+                'title.required' => 'Please input unique Title',
+                'year.required' => 'Please select Year',
+                'abstract.required' => 'Please enter Abstract',
+                'members.required' => 'Please enter members',
+                'adviser.required' => 'Please input your Adviser',
+                'banner_path.required' => 'Please upload sample Image',
+                'document_path.required' => 'Please Attached document',
+                'click_checkbox.required' => 'Please click a Term and Privacy Policy'
 
-        //     ]);
+            ]);
 
-        //     if ($validator->fails()) {
-        //         return response()->json(['errors' => $validator->errors()]);
-        //     }
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()]);
+            }
 
         // $archive = new archive;
         // $banner_path = "";
