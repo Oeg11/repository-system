e4c44b34732a7e1ebe5156c348133bb8930a60db
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-09 21:30:09 +0800

    wew
    ewe

--- resources/views/staff/modal/archiveupdatestatus.blade.php
+++ resources/views/staff/modal/archiveupdatestatus.blade.php
@@ -36,3 +36,15 @@
       </div>
     </div>
   </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script type="text/javascript">
+      $(function () {
+          $("#edit_status").change(function () {
+              if ($(this).val() === "0") {
+                  $("#remark").show();
+              } else {
+                  $("#remark").hide();
+              }
+          });
+      });
+  </script>
