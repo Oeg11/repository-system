f5aea64c264a1eae95ce58a4920030ee67eb0694
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-09 21:05:01 +0800

    fdg

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -116,7 +116,7 @@ class StudentController extends Controller
             $isChecked = $request->input('click_checkbox');
 
             if ($validator->fails()) {
-                return response()->json(['errors' => $validator->errors(), 'errors' => $isChecked]);
+                return response()->json(['errors' => $validator->errors()]);
             }
 
         $archive = new archive;
