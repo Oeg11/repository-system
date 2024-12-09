24f79dfe661ddf38aa779f28f73d51ab5851ad5b
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-09 21:32:48 +0800

    dwd
    dwd

--- resources/views/staff/modal/archiveupdatestatus.blade.php
+++ resources/views/staff/modal/archiveupdatestatus.blade.php
@@ -36,7 +36,7 @@
       </div>
     </div>
   </div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script type="text/javascript">
       $(function () {
           $("#edit_status").change(function () {
@@ -47,4 +47,4 @@
               }
           });
       });
-  </script>
+  </script> --}}
