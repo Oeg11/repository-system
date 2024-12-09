1475aadbf32b8aaeb0f5dc269df3523dd0c3aee6
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-09 14:35:30 +0800

    dsf

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1423,7 +1423,7 @@ class AdminController extends Controller
                 ->leftjoin('curricula','curricula.id','=','archives.curriculum_id')
                 ->leftjoin('departments','departments.id','=','archives.department_id')
                 ->where('archives.type',  $type)
-                ->whereBetween('archives.create_at', [$date1, $date2])
+                ->whereBetween('archives.created_at', [$date1, $date2])
                ->get();
 
 
@@ -1458,7 +1458,7 @@ class AdminController extends Controller
                 ->leftjoin('curricula','curricula.id','=','archives.curriculum_id')
                 ->leftjoin('departments','departments.id','=','archives.department_id')
                 ->where('archives.type',  $type)
-                ->whereBetween('archives.create_at', [$date1, $date2])
+                ->whereBetween('archives.created_at', [$date1, $date2])
                ->get();
             }
 
