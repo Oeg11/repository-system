96c90cbe3efcf359b9cb965d5c61f93ecd515cf0
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 15:20:29 +0800

    rwr

--- resources/views/staff/flayouts/sidebar.blade.php
+++ resources/views/staff/flayouts/sidebar.blade.php
@@ -83,7 +83,7 @@
       <a href="{{route('staff.dashboard')}}" class="brand-link">
 
         <!--<img src="{{ asset('assets/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">-->
-        <span class="brand-text font-weight-light">Faculty Panel</span>
+        <span class="brand-text font-weight-light"><center>Faculty Panel</center></span>
       </a>
 
       <!-- Sidebar -->
