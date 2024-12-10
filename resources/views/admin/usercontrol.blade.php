b923df1849b8340d553284e55f16cbb4d47a6eac
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 18:54:32 +0800

    ewe

--- resources/views/admin/usercontrol.blade.php
+++ resources/views/admin/usercontrol.blade.php
@@ -30,7 +30,7 @@
             <div class="card mt-4">
                 <div class="card-body">
                     <form method="POST">
-                        <h3 class="text-center">User Control</h3>
+                        <div class="col-md-10"><h2>User Control</h2></div>
                         <hr>
                         {{-- <select class="form-control" aria-label="Default select example">
                             <option selected="true" disabled="disabled"> &larr; Select Staff &rarr;</option>
