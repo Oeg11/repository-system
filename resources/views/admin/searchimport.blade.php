e59a2424a78d4812768f04a7dc61b865a69d5062
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 18:35:13 +0800

    ew
    ewe

--- resources/views/admin/searchimport.blade.php
+++ resources/views/admin/searchimport.blade.php
@@ -28,7 +28,7 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-                 <h2>Search Type Reports</h2>
+                 <h2>Type Reports</h2>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
