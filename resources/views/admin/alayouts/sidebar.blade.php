b6df5f73bfa1db85c0b94682bfa05e0e66cfbbb6
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 15:23:57 +0800

    ew

--- resources/views/admin/alayouts/sidebar.blade.php
+++ resources/views/admin/alayouts/sidebar.blade.php
@@ -82,7 +82,7 @@
       <a href="{{route('admin.dashboard')}}" class="brand-link">
 
         <!--<img src="{{ asset('assets/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">-->
-        <span class="brand-text font-weight-light">STI Panel</span>
+        <span class="brand-text font-weight-light"><center>Admin Panel</center></span>
       </a>
 
       <!-- Sidebar -->
