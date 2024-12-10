b3ac7e86a619c14d146c26ebce3a363894b200ef
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 15:17:06 +0800

    wewe

--- resources/views/students/slayouts/sidebar.blade.php
+++ resources/views/students/slayouts/sidebar.blade.php
@@ -83,7 +83,7 @@
       <a href="{{route('students.index')}}" class="brand-link">
 
         <!--<img src="{{ asset('assets/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">-->
-        <span class="brand-text font-weight-light">STI Panel</span>
+        <span class="brand-text font-weight-light"><center>Student Panel</center></span>
       </a>
 
       <!-- Sidebar -->
