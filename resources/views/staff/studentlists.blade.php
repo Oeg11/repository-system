3fa5e9e0ac0bddc690e19058c0dfed0c434b6100
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 21:07:23 +0800

    ewqe

--- resources/views/staff/studentlists.blade.php
+++ resources/views/staff/studentlists.blade.php
@@ -8,12 +8,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Dashboard</h1>
+            {{-- <h1>Dashboard</h1> --}}
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('staff.dashboard')}}">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
+              {{-- <li class="breadcrumb-item active">Dashboard</li> --}}
             </ol>
           </div>
         </div>
@@ -28,7 +28,7 @@
             <div class="card">
               <div class="card-header">
                 <div class="row">
-                    <div class="col-md-10">List of Students</h2></div>
+                    <div class="col-md-10"><h2><b>List of Students</b></h2></div>
 
                 </div>
               </div>
