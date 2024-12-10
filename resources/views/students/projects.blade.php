e42d2f74d10a00a7d4307bac2092c8f01142bc68
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-10 15:40:52 +0800

    rewr

--- resources/views/students/projects.blade.php
+++ resources/views/students/projects.blade.php
@@ -47,7 +47,8 @@
 
                                 <p>
                                     <span style="color:#8c8c8c">By</span> <span style="color:#007bff">{{
-                                        Str::ucfirst($item->fullname) }}</span>
+                                        Str::ucfirst($item->fullname) }}</span> <span style="color:#007bff">{{
+                                            Str::ucfirst($item->name) }}</span>
                                     <br>
                                     <p class="mb-0 w-100"style="color:#8c8c8c;font-size:11px">
                                        {{
