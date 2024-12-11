1c972bc824544ee0c768151313e413d2009f3c35
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2024-12-11 13:15:27 +0800

    df

--- resources/views/admin/alayouts/sidebar.blade.php
+++ resources/views/admin/alayouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="wrapper">
 
     <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
+    {{-- <div class="preloader flex-column justify-content-center align-items-center">
        <img class="animation__shake" src="{{asset('assets/img/131507_hacker_administrator_system_sys_hack_icon.png')}}"  height="60" width="60" style="border-radius: 50%">
-     </div>
+     </div> --}}
 
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -81,7 +81,7 @@
       <!-- Brand Logo -->
       <a href="{{route('admin.dashboard')}}" class="brand-link">
 
-        <!--<img src="{{ asset('assets/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">-->
+        <img src="{{asset('assets/img/131507_hacker_administrator_system_sys_hack_icon.png')}}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><center>Admin Panel</center></span>
       </a>
 
