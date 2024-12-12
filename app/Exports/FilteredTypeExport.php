1c133cdddc5cc10d178f0b5b71a9778bebc5ad1a
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-12 17:32:29 +0800

    weqe

--- app/Exports/FilteredTypeExport.php
+++ app/Exports/FilteredTypeExport.php
@@ -11,8 +11,30 @@ class FilteredTypeExport implements FromCollection
     /**
     * @return \Illuminate\Support\Collection
     */
-    // public function collection()
-    // {
-    //     return archive::select("id", "fullname", "email", "department_id", "curriculum_id", "role", "status")->get();
-    // }
+    protected $filters;
+
+    public function __construct($filters)
+    {
+        $this->filters = $filters;
+    }
+
+    public function view(): View
+    {
+        $query = archive::query();
+
+        // Apply filters
+        if (!empty($this->filters['type'])) {
+            $query->where('type', 'like', '%' . $this->filters['type'] . '%');
+        }
+
+        // if (!empty($this->filters['status'])) {
+        //     $query->where('status', $this->filters['status']);
+        // }
+
+        $data = $query->get();
+
+        return view('exports.filtered-data', [
+            'data' => $data,
+        ]);
+    }
 }
