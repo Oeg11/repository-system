1118dbea16d3278a49795ccc4abf9dd34719b47c
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-15 14:58:05 +0800

    ewqe

--- resources/views/reports/ExportexcelcategoryReport.blade.php
+++ resources/views/reports/ExportexcelcategoryReport.blade.php
@@ -6,13 +6,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Category Report</title>
-    <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="fa fa-user"></i>Exported by: {{ Auth::user()->email }}
-          <br>
-          {{-- <i class="fa fa-user"></i>Exported Date: {{ Carbon::now()->currentDateTime}} --}}
-        </a>
-    </li>
+    <div class="header">
+        <p><strong>Exported by:</strong> {{ Auth::user()->email }}</p>
+        {{-- <p><strong>Exported at:</strong> {{ $exportedAt }}</p> --}}
+    </div>
     <style>
         /* @import url('https://fonts.googleapis.com/css2?family=Afacad&display=swap');
          body{
