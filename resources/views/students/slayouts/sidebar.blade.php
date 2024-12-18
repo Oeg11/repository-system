fcfb09159659652c4f7675ded7295d29f433d50a
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-19 04:15:37 +0800

    ewqe

--- resources/views/students/slayouts/sidebar.blade.php
+++ resources/views/students/slayouts/sidebar.blade.php
@@ -94,7 +94,7 @@
 
 
         <!-- Sidebar Menu -->
-        <nav class="mt-2">
+        <nav class="mt-2" id="myDIV_">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
