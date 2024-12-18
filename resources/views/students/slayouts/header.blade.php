b0630c9785489db5b55daada034ef1debf3e15e3
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-19 03:06:40 +0800

    ewq

--- resources/views/students/slayouts/header.blade.php
+++ resources/views/students/slayouts/header.blade.php
@@ -63,6 +63,16 @@
     /* pointer-events: auto;
     overflow: auto; */
 }
+
+.overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(255, 255, 255, 0); /* Transparent overlay */
+            z-index: 10; /* Ensure overlay is on top */
+        }
 </style>
 
 </head>
