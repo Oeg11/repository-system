9c8e116f41f8d10ae0b771ddaa28050eaac09472
Oeg11 <97080297+Oeg11@users.noreply.github.com>
2024-12-19 03:55:30 +0800

    ew

--- resources/views/students/slayouts/sidebar.blade.php
+++ resources/views/students/slayouts/sidebar.blade.php
@@ -149,3 +149,15 @@
       </div>
       <!-- /.sidebar -->
     </aside>
+    <script>
+        // Add active class to the current button (highlight it)
+        var header = document.getElementById("myDIV_");
+        var btns = header.getElementsByClassName("btns_");
+        for (var i = 0; i < btns.length; i++) {
+          btns[i].addEventListener("click", function() {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+          });
+        }
+ </script>
